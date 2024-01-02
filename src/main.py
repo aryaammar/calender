f9e589c50329e883ce040f2b8f06eb044ff1ee6f
root <root@localhost>
2024-01-02 21:35:35 +0800

    auto publish 1704202534136

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#mengimpor modul calendar
+import calendar
+
+#menginput tahun dan bulan
+yy = int(input("2007: "))
+mm = int(input("11: "))
+
+# Menampilkan Kalender Bulanan
+print(calendar.month(yy, mm))
